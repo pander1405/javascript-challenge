b71d9aaaf30bcf1ebb4f86d37403fd5cc7cac791
Paul Anderson <paulanderson1991@gmail.com>
2021-03-08 16:47:30 -0500

    UFO level 1 mostly complete. Needs review

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,45 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
-// YOUR CODE HERE!
+// d3 to select table body
+var tbody = d3.select("tbody");
+
+// UFO Sighting values for each column
+tableData.forEach(function(ufoSighting) {
+    // Append table row `tr` per from from tableData
+    var row = tbody.append("tr");
+
+    // Use `Object.entries` to append td for each value
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+});
+
+
+// Select the button
+var button = d3.select("#filter-btn");
+button.on("click", function() {
+
+    tbody.html("");
+
+
+    var element = d3.select("#datetime");
+
+    var inputValue = element.property("value");
+    // Filter datetime
+    var filteredData = tableData.filter(event => event.datetime === inputValue);
+
+    filteredData.forEach(function(filteredData) {
+
+    // Repeat same as above for filtered data
+    var row = tbody.append("tr");
+
+    Object.entries(filteredData).forEach(function([key, value]) {
+
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+});
